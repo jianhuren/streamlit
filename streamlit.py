211c457aecfa0dfd4d0eecc99cc1638f50b8f7df
jianhuren <156560833+jianhuren@users.noreply.github.com>
2024-01-14 12:31:28 +0800

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -2,17 +2,7 @@ import pandas as pd
 from datetime import time
 import streamlit as st
 
-# st.title('Hello World')
-data = pd.read_excel('/Users/mac/Desktop/Workbook.xlsx',sheet_name='Sheet1')
-a = int(max(data['红包剩余金额']))
-print(a)
-st.slider('金额大小',0,a,(0,int(a/2)),key='start')
-st.session_state.start[0]
-print(type(st.session_state.start[0]))
-st.slider('时间选择',value=(time(11,30),time(9,30)))
-# df = pd.DataFrame(data[(data['红包剩余金额']>=st.session_state.start[0]) & (data['红包剩余金额']<=st.session_state.start[1])])
-# st.text(len(df))
-# st.write(df)
+
 st.selectbox('选择',[1,2,3])
 
 
